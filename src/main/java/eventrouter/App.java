0c8d13b109b2cc86a9d6e7743800bbbb66bc4d5e
Rodion Gorkovenko <rodiongork@github.com>
2015-04-22 09:12:46 +0300

    Using https (forgotten file)

--- src/main/java/eventrouter/App.java
+++ src/main/java/eventrouter/App.java
@@ -5,11 +5,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -22,11 +28,32 @@ public class App {
     }
 
     public void run() throws Exception {
-        Server server = new Server(8080);
+        Server server = createHttpsServer();
         //server.setHandler(new CustomHandler());
         server.setHandler(createHandler(createSpringContext()));
         server.start();
         server.join();
+    }
+    
+    private Server createBasicServer() {
+        return new Server(8080);
+    }
+    
+    private Server createHttpsServer() {
+        Server server = new Server();
+        HttpConfiguration https = new HttpConfiguration();
+        https.addCustomizer(new SecureRequestCustomizer());
+        SslContextFactory sslCtxFactory = new SslContextFactory();
+        sslCtxFactory.setKeyStorePath(this.getClass().getResource("/keystore.jks").toExternalForm());
+        sslCtxFactory.setKeyStorePassword("123456");
+        sslCtxFactory.setKeyManagerPassword("123456");
+        
+        ServerConnector conn = new ServerConnector(server);
+        conn.setPort(8080);
+        ServerConnector conns = new ServerConnector(server, new SslConnectionFactory(sslCtxFactory, "http/1.1"), new HttpConnectionFactory(https));
+        conns.setPort(8083);
+        server.setConnectors(new ServerConnector[] {conn, conns});
+        return server;
     }
 
     public static class CustomHandler extends AbstractHandler {
